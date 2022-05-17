bdd4140121f173a5aa597d65f43343a864019d54
Tatu Peltola <tatu.peltola@gmail.com>
2022-05-17 23:00:28 +0300

    Improve recording scripts

--- record.sh
+++ record.sh
@@ -2,6 +2,7 @@
 # Read audio sent by input.sh
 # and record it to a file.
 
-ffmpeg \
+mkdir -p ../recordings/
+exec ffmpeg \
 	-f s24be -ar 48000 -ac 2 -i zmq:tcp://127.0.0.1:42011 \
-	-acodec pcm_s24le test_$(date +%s).wav
+	-acodec flac "../recordings/recording_$(date -Iseconds).flac"
--- record_processed.sh
+++ record_processed.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-# Read audio sent by input.sh
+# Read audio sent by process.sh
 # and record it to a file.
 
-ffmpeg \
+mkdir -p ../recordings/
+exec ffmpeg \
 	-f s24be -ar 48000 -ac 2 -i zmq:tcp://127.0.0.1:42012 \
-	-acodec pcm_s24le test_processed_$(date +%s).wav
+	-acodec flac "../recordings/recording_processed_$(date -Iseconds).flac"
