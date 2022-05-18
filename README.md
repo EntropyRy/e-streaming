ce137b8449002a2cebf5fc22252227c733fe296f
Tatu Peltola <tatu.peltola@gmail.com>
2022-05-18 17:02:00 +0300

    Write some service files

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
 # e-streaming
 Software and configuration on the streaming machine
 
-The address and password of the streaming server are not kept in
+# Installation and configuration
+## Icecast address
+
+The address and password of the Icecast server are not kept in
 this repository. Please create `icecast_address.sh` like:
 
     ICECAST_ADDRESS=source:PASSWORD@ADDRESS:PORT/NAME
 
 Where PASSWORD is the source password, ADDRESS:PORT is the address
 and NAME forms part of the name of the stream.
+
+## Services
+
+To install the services, `cd` to this directory and do:
+
+    ./install_services.sh
+
+Check that audio input and processing are working:
+
+    journalctl --user -u es_input.service
+    journalctl --user -u es_process.service
+
+If they didn't start at all, you might have to do:
+
+    systemctl --user start default.target
+
+To start streaming, do:
+
+    systemctl --user start es_stream.target
+
--- install_services.sh
+++ install_services.sh
@@ -1,38 +1,74 @@
 #!/bin/sh
-SERVICES="input process icecast_unprocessed icecast_opus"
-SERVICE_DEST="${HOME}/.config/systemd/user/"
+# Services that can always be running
+SERVICES_ALWAYS="input process"
+# Services that are started when streaming is on
+SERVICES_STREAM="icecast_unprocessed icecast_opus"
+# Services to be enabled
+SERVICES_ENABLED="${SERVICES_ALWAYS} ${SERVICES_STREAM}"
 
-mkdir -p "${SERVICE_DEST}"
+
+# Paths
+SERVICE_DIR="${HOME}/.config/systemd/user"
+SCRIPT_DIR="$(pwd)"
+
+mkdir -p "${SERVICE_DIR}"
+
 
 write_service()
 {
 	SERVICE_NAME="es_$1.service"
-	SCRIPT_FILE="$(pwd)/$1.sh"
-	cat > "${SERVICE_DEST}${SERVICE_NAME}"  <<EOF
-# TODO: service file here for ${SCRIPT_FILE}
+	SERVICE_FILE="${SERVICE_DIR}/${SERVICE_NAME}"
+	SCRIPT_FILE="${SCRIPT_DIR}/$1.sh"
+	echo Writing "${SERVICE_FILE}"
+	cat > "${SERVICE_FILE}"  <<EOF
+[Unit]
+Description=e-streaming $1
+
+[Service]
+WorkingDirectory=${SCRIPT_DIR}
+ExecStart=${SCRIPT_FILE}
+
+StandardOutput=journal
+StandardError=journal
+
+Restart=always
+
+[Install]
+WantedBy=$2
+
 EOF
 }
 
 enable_service()
 {
 	SERVICE_NAME="es_$1.service"
+	echo Enabling "${SERVICE_NAME}"
 	systemctl --user enable "${SERVICE_NAME}"
 }
 
 
 set -e
 
-for name in ${SERVICES}
+for name in ${SERVICES_ALWAYS}
 do
-	write_service "$name"
+	write_service "$name" default.target
 done
 
-echo "Services files written. Running daemon-reload"
+for name in ${SERVICES_STREAM}
+do
+	write_service "$name" es_stream.target
+done
+
+cat > "${SERVICE_DIR}/es_stream.target" <<EOF
+[Unit]
+Description=e-streaming on
+EOF
+
+echo "Service files written. Running daemon-reload"
 systemctl --user daemon-reload
 
-for name in ${SERVICES}
+for name in ${SERVICES_ENABLED}
 do
-	echo "Enabling service for $name"
 	enable_service "$name"
 done
 
