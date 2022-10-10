a957c16cdd2a67b08f061de81131713ce6aa6407
Tatu Peltola <tatu.peltola@gmail.com>
2022-10-10 17:26:30 +0300

    Change sound card

--- process.sh
+++ process.sh
@@ -3,7 +3,7 @@
 # Read audio sent by input.sh,
 # process it and send to another ZeroMQ address.
 
-level_in=7
+level_in=3
 ffmpeg \
 	-f s24be -ar 48000 -ac 2 -i zmq:tcp://127.0.0.1:42011 \
 	-filter "alimiter=level_in=${level_in}:limit=0.95" \
