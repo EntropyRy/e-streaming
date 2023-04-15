bf0259b55eb3a3bb568cbdf52acb7d2aaa6681ba
Tatu Peltola <tatu.peltola@gmail.com>
2023-04-15 16:57:16 +0300

    Change sample format in messages to s32le
    s24be was used mainly because RTP (used for initial experiments)
    did not support s32le format.
    s32le probably makes more sense for most software
    and works fine with ZeroMQ.

--- common.sh
+++ common.sh
@@ -4,13 +4,13 @@
 
 
 # ffmpeg parameters to publish input audio from soundcard
-PUB_INPUT='-f s24be zmq:tcp://*:42011'
+PUB_INPUT='-f s32le zmq:tcp://*:42011'
 # ffmpeg parameters to subscribe to above
-SUB_INPUT='-f s24be -ar 48000 -ac 2 -i zmq:tcp://127.0.0.1:42011'
+SUB_INPUT='-f s32le -ar 48000 -ac 2 -i zmq:tcp://127.0.0.1:42011'
 
 # ffmpeg parameters to publish processed audio
-PUB_PROCESSED='-f s24be zmq:tcp://*:42012'
+PUB_PROCESSED='-f s32le zmq:tcp://*:42012'
 # ffmpeg parameters to subscribe to above
-SUB_PROCESSED='-f s24be -ar 48000 -ac 2 -i zmq:tcp://127.0.0.1:42012'
+SUB_PROCESSED='-f s32le -ar 48000 -ac 2 -i zmq:tcp://127.0.0.1:42012'
 
 
--- input.sh
+++ input.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 # Read audio from the soundcard and distribute it by ZeroMQ.
-# The PCH soundcard supports S16LE and S32LE formats, but the 32-bit data
-# seems to be sampled with 24 bits, with the lowest 8 bits always zero.
-# Thus, it can be converted to 24-bit samples without losing anything.
-# TODO: Check if above is true for the DGX soundcard as well.
-# At least it supports the same formats.
 
 . ./common.sh
 
