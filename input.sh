22cf2a59d0d4495f51f62eb75d37c56f56a03e8e
Tatu Peltola <tatu.peltola@gmail.com>
2025-10-24 20:10:03 +0300

    Change sound card to Traktor Audio 6

--- input.sh
+++ input.sh
@@ -3,6 +3,12 @@
 
 . ./common.sh
 
+# Traktor Audio 6 USB audio interface is hw:CARD=T6,DEV=0
+# It has 6 channels and does not directly support 2-channel recording.
+# ALSA will convert it to stereo if "plughw" is used instead of "hw".
+# A better way might be to use ffmpeg (or whatever)
+# to pick the correct channels from those 6.
+# This works though.
 exec ffmpeg \
-	-f alsa -acodec pcm_s32le -i hw:CARD=DGX,DEV=0 \
+	-f alsa -acodec pcm_s32le -i "plughw:CARD=T6,DEV=0" \
 	$PUB_INPUT
